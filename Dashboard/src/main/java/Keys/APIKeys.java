78bf10c5cfdfe013b51be7e6192b21e589a13984
Eike Schaubeck <eike.schaubeck@stud-mail.uni-wuerzburg.de>
2020-08-12 12:16:02 +0200

    removed API-Keys

--- Dashboard/src/main/java/Keys/APIKeys.java
+++ Dashboard/src/main/java/Keys/APIKeys.java
@@ -5,8 +5,9 @@ import com.google.maps.GeoApiContext;
 public class APIKeys {
 
     //Transit Keys:
+    //please insert own Google-API-Key
     public static String getGoogleKey() {
-        return "AIzaSyAGQiTWX-Cf3IwTlyl5S7Wp8BEjCpz_cY8";
+        return "";
     }
     public static GeoApiContext getApiGoogleContext() {
         GeoApiContext.Builder contextHelper = new GeoApiContext.Builder().apiKey(getGoogleKey());
@@ -14,11 +15,13 @@ public class APIKeys {
     }
 
     //IPGeo Key:
+    //please insert own IPGeo-Key
     public static String getIPGeoKey() {
         return "30dab8a36fa54c0a987c733d50f9f804";
     }
 
     //Weather Key
+    //please insert own DarkSky-Key
     public static String getDarkSkyKey(){
         return "5640e4e5d97b320808f494bb468a2ac2";
     }
